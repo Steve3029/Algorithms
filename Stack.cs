1c819965a2c5bfce1639086e7cf02e76243e3c48
Steve <steve.liu.3029@gmail.com>
2019-07-13 16:22:58 +1200

    first commit

---
+++ Stack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class Stack
+    {
+        private int capacity;
+        private readonly int minCapacity;
+        private int[] data;
+        private int top = -1;
+
+        public Stack() : this(1000)
+        {
+        }
+
+        public Stack(int size)
+        {
+            data = new int[size];
+            capacity = minCapacity = size;
+        }
+
+        public bool IsEmpty
+        {
+            get { return (top == -1); }
+        }
+
+        public int Size()
+        {
+            return (top + 1);
+        }
+
+        public void Print()
+        {
+            foreach (var item in data)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        public void Push(int value)
+        {
+            if (Size() == capacity)
+            {
+                var newData = new int[capacity * 2];
+                Array.Copy(data, 0, newData, 0, capacity);
+                data = newData;
+                capacity *= 2;
+            }
+            top++;
+            data[top] = value;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty) throw new InvalidOperationException("StackEmptyException");
+
+            int topVal = data[top];
+            top--;
+            if (Size() == capacity/2 && capacity > minCapacity)
+            {
+                capacity /= 2;
+                var newData = new int[capacity];
+                Array.Copy(data, 0, newData, 0, capacity);
+                data = newData;
+            }
+            return topVal;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty) throw new InvalidOperationException("StackEmptyException");
+            return data[top];
+        }
+    }
+}
---
+++ StackLL.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class StackLL
+    {
+        private Node head = null;
+        private int count = 0;
+
+        private class Node
+        {
+            internal int value;
+            internal Node next;
+
+            public Node(int v, Node n)
+            {
+                value = v;
+                next = n;
+            }
+        }
+
+        public int Size()
+        {
+            return count;
+        }
+
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+
+        public void Print()
+        {
+            var temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.value + " ");
+                temp = temp.next;
+            }
+        }
+
+        public void Push(int value)
+        {
+            head = new Node(value, head);
+            count++;
+        }
+
+        public int Pop()
+        {
+            if(IsEmpty) throw new InvalidOperationException("StackEmptyException");
+
+            var topVal = head.value;
+            var temp = head.next;
+            head.next = null;
+            head = temp;
+            count--;
+            return topVal;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty) throw new InvalidOperationException("StackEmptyException");
+
+            return head.value;
+        }
+
+        public void InsertAtBottom(int value)
+        {
+            if (IsEmpty)
+            {
+                Push(value);
+            }
+            else
+            {
+                int temp = Pop();
+                InsertAtBottom(value);
+                Push(temp);
+            }
+        }
+    }
+}
