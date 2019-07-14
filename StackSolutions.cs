13816b0f785f415cad80c06dc156b0330e35c675
Steve <steve.liu.3029@gmail.com>
2019-07-15 11:44:22 +1200

    add a solution for sorting a stack without extra storage space

---
+++ StackSolutions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class StackSolutions
+    {
+        public static void SortStack(LinkedListStack stack)
+        {
+            int temp;
+            if (stack.Size() > 0)
+            {
+                temp = stack.Pop();
+                SortStack(stack);
+                SortedInsert(stack, temp);
+            }
+        }
+
+        public static void SortedInsert(LinkedListStack stack, int value)
+        {
+            if(stack.Size() == 0 || value > stack.Peek())
+            {
+                stack.Push(value);
+            }
+            else
+            {
+                var temp = stack.Pop();
+                SortedInsert(stack, value);
+                stack.Push(temp);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            var stack = new LinkedListStack();
+            stack.Push(5);
+            stack.Push(2);
+            stack.Push(7);
+            stack.Push(1);
+            stack.Push(9);
+            SortStack(stack);
+            stack.Print();
+        }
+    }
+}
